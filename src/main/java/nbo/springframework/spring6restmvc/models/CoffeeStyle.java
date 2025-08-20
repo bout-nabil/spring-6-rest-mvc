d148b3cce8ccfb4e0a5a6e7aef4b17b838b7a197
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-08-21 00:07:20 +0100

    Update CoffeeStyle name

--- src/main/java/nbo/springframework/spring6restmvc/models/CoffeeStyle.java
+++ src/main/java/nbo/springframework/spring6restmvc/models/CoffeeStyle.java
@@ -1,5 +1,36 @@
 package nbo.springframework.spring6restmvc.models;
 
-public class CoffeeStyle {
+public enum CoffeeStyle {
+    // Classic
+    ESPRESSO,
+    AMERICANO,
+    CAPPUCCINO,
+    MACCHIATO,
+    MOCHA,
+    LATTE,
+    RISTRETTO,
+    AFFOGATO,
 
+    // Strong / Bold
+    BLACK_BREW,
+    DARK_ROAST,
+    DOUBLE_SHOT,
+    NITRO,
+    BOLD_BEAN,
+    ROCKET_FUEL,
+
+    // Sweet / Smooth
+    CARAMEL_DRIZZLE,
+    VANILLA_BEAN,
+    HAZELNUT_BLISS,
+    CINNAMON_SWIRL,
+    HONEY_ROAST,
+
+    // Creative / Fancy
+    JAVA_JOLT,
+    BEANSTORM,
+    CAFFEINE_FIX,
+    MORNING_GRIND,
+    MIDNIGHT_ROAST,
+    STEAM_AND_CREAM;
 }
