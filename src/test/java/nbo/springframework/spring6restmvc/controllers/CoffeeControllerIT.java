92e4be397ef0b348f801763321d6e6ac442fd72b
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-09-14 11:37:28 +0100

    JPA save Operation

--- src/test/java/nbo/springframework/spring6restmvc/controllers/CoffeeControllerIT.java
+++ src/test/java/nbo/springframework/spring6restmvc/controllers/CoffeeControllerIT.java
@@ -6,12 +6,14 @@ import nbo.springframework.spring6restmvc.repositories.ICoffeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CoffeeControllerIT {
@@ -19,6 +21,25 @@ class CoffeeControllerIT {
     ICoffeeRepository iCoffeeRepository;
     @Autowired
     CoffeeController coffeeController;
+
+    @Rollback
+    @Transactional
+    @Test
+    void testSaveNewCoffee(){
+        CoffeeDTO coffeeDTO = CoffeeDTO.builder()
+                .nameCoffee("Test Coffee")
+                .build(); // Using builder pattern to create a Coffee object with name "Test Coffee"
+        ResponseEntity responseEntity = coffeeController.createdCoffee(coffeeDTO); // Using the controller to create a new coffee
+
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+        assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
+
+        String[] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
+        UUID savedUUID = UUID.fromString(locationUUID[locationUUID.length -1]);
+
+        Coffee coffee = iCoffeeRepository.findById(savedUUID).get();
+        assertThat(coffee).isNotNull();
+    }
 
     @Test
     void testCoffeeNotFound(){
